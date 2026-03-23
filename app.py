03204efd25dca2d7d3fdcb554f82cbaefb039303
Chris <24007@burnside.school.nz>
2026-03-23 14:33:33 +1300

    2 commit (database set up, no data)

--- app.py
+++ app.py
@@ -4,14 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, world!"
-
-
-@app.route('/admin')
-def admin():
-    return "<h1>Hello admin</h1>"
-
-
+    #home page
+    return "Hello Again!"
 
 if __name__ == "__main__":
     app.run(debug=True)
