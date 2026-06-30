68fe0939429cb2f8284091082f3d08076a2196a2
Chris <24007@burnside.school.nz>
2026-06-30 13:18:50 +1200

    39 commit | just a few comments/finishing off

--- static/search.js
+++ static/search.js
@@ -1,7 +1,9 @@
+//Find all the card elements on the page that we want to show or hide when someone types in the search bar
 const optionsEls = document.querySelectorAll(".w3-card-4");
+// Selects the search inputs
 const searchEl = document.getElementById("page-search");
 
-
+// The search function code that makes it work
 const search = () => 
     optionsEls.forEach(
         (e) =>
@@ -12,5 +14,5 @@ const search = () =>
                     ? ""
                     : "none")
     );
-
+// This makes the search function every time the user inputs or clears it
 searchEl.addEventListener("input", search);
